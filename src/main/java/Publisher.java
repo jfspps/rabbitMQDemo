17884e4640cb7744930c95cad4f412b1a34b52f6
jfspps <jfsppsDev@gmail.com>
2021-08-16 22:25:29 +0100

    first commit

---
+++ src/main/java/Publisher.java
@@ -0,0 +1,23 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+public class Publisher {
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        Connection connection = connectionFactory.newConnection();
+
+        // set up a queue, SomeQueue
+        Channel channel = connection.createChannel();
+        String message = "Some Rabbit down the hole";
+        channel.basicPublish("", "SomeQueue", null, message.getBytes(StandardCharsets.UTF_8));
+
+        channel.close();
+        connection.close();
+    }
+}
